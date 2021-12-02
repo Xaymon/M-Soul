de268e51a714bc6af7532e420688d32c54b6e586
keofc <keoitc2014@gmail.com>
2021-12-02 12:13:15 +0700

    transfer

--- app/manage.py
+++ app/manage.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for,session,jsonify, json
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, json
 import psycopg2
 from app import app
 from kk_con import *
@@ -6,9 +6,41 @@ from kk_con import *
 
 @app.route('/manageexrate')
 def manageexrate():
-    # cur = gobal.con.cursor()
-    # sql="SELECT curency_code, curency_name, buy, sale FROM public.exchange_rate where date_end isnull order by roworder "
-    # cur.execute(sql)
-    # rate_ = cur.fetchall()
+    cur = gobal.con.cursor()
+    sql = "SELECT to_char(date_start,'DD-MM-YYYY'),curency_code, curency_name, buy, sale,roworder FROM public.exchange_rate order by roworder "
+    cur.execute(sql)
+    rate_ = cur.fetchall()
 
-    return render_template('manage/index.html')
+    return render_template('manage/addrate.html', rate_=rate_)
+
+
+@app.route('/saverate', methods=['POST'])
+def saverate():
+    currency_code = request.form['currency_code']
+    buy = request.form['buy']
+    sale = request.form['sale']
+    curen_ = ""
+    if currency_code == '01':
+        curen_ = 'THB'
+    elif currency_code == '02':
+        curen_ = 'USD'
+    else:
+        curen_ = 'THB-USD'
+    data = (currency_code, curen_, buy, sale)
+    cur = gobal.con.cursor()
+    sql = "update exchange_rate set date_end=LOCALTIMESTAMP(0) where date_end isnull and curency_code=%s"
+    cur.execute(sql, (currency_code,))
+    curs = gobal.con.cursor()
+    sqls = "INSERT INTO exchange_rate( curency_code, curency_name, buy, sale, date_start) VALUES (%s,%s,%s,%s,LOCALTIMESTAMP(0));"
+    curs.execute(sqls, (data))
+    gobal.con.commit()
+    return redirect(url_for('manageexrate'))
+
+
+@app.route('/rate_delete/<string:id>')
+def rate_delete(id):
+    cur = gobal.con.cursor()
+    sql = "delete from exchange_rate where roworder=%s"
+    cur.execute(sql, (id,))
+    gobal.con.commit()
+    return redirect(url_for('manageexrate'))
