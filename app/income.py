436da071ba0820aec49f4be08330b76ff99b5c96
Xaymon <60602154+Xaymon@users.noreply.github.com>
2021-12-07 16:35:37 +0700

    In*Outcome

---
+++ app/income.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, json
+import psycopg2
+from app import app
+from kk_con import *
+
+
+@app.route('/income')
+def income():
+    with gobal.con:
+        if not session.get("name"):
+            return redirect("/login")
+        else:
+            cur = gobal.con.cursor()
+            sql = """
+                    SELECT roworder,bill_no, item_name, to_char(bill_date,'DD-MM-YYYY HH24:MI:SS') as bill_date, cash_kip, cash_baht, cash_dollar FROM public.tb_income order by roworder DESC
+                  """
+
+                    # SELECT roworder,bill_no, item_name, bill_date, cash_kip, cash_baht, cash_dollar FROM public.tb_outcome order by roworder DESC
+                    # VALUES(%s,%s,%s, LOCALTIMESTAMP(0),%s,%s,%s)"""
+            cur.execute(sql)
+            rate_trans = cur.fetchall()
+            return render_template('In-Outcome/income.html', rate_trans=rate_trans)
+
+@app.route('/save_income', methods=['POST'])
+def save_income():
+    with gobal.con:
+        cur = gobal.con.cursor()
+        if not session.get("name"):
+            return redirect("/login")
+        else:
+            sql = """INSERT INTO public.tb_income (item_name, cash_kip, cash_baht, cash_dollar, bill_date)
+                     VALUES(%s,%s,%s,%s, LOCALTIMESTAMP(0))
+                  """
+            item_name = request.form['item_name']
+            cash_kip = request.form['cash_kip']
+            cash_baht = request.form['cash_baht']
+            cash_dollar = request.form['cash_dollar']
+            bill_date = format(request.form['bill_date'])
+
+            data = (item_name, cash_kip, cash_baht, cash_dollar, bill_date)
+            print(data)
+            cur.execute(sql, (data))
+            gobal.con.commit()
+            return redirect(url_for('income'))
+
+@app.route('/income_delete/<string:id>')
+def income_delete(id):
+    if not session.get("name"):
+        return redirect("/login")
+    else:
+        print(id)
+        cur = gobal.con.cursor()
+        sql = "delete from public.tb_income where roworder=%s"
+        cur.execute(sql, (id,))
+        gobal.con.commit()
+        return redirect(url_for('income'))
+
---
+++ app/outcome.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify, json
+import psycopg2
+from app import app
+from kk_con import *
+
+
+@app.route('/outcome')
+def outcome():
+    with gobal.con:
+        if not session.get("name"):
+            return redirect("/login")
+        else:
+            cur = gobal.con.cursor()
+            sql = """
+                    SELECT roworder,bill_no, item_name, to_char(bill_date,'DD-MM-YYYY HH24:MI:SS') as bill_date, cash_kip, cash_baht, cash_dollar FROM public.tb_outcome order by roworder DESC
+                  """
+
+                    # SELECT roworder,bill_no, item_name, bill_date, cash_kip, cash_baht, cash_dollar FROM public.tb_outcome order by roworder DESC
+                    # VALUES(%s,%s,%s, LOCALTIMESTAMP(0),%s,%s,%s)"""
+            cur.execute(sql)
+            rate_trans = cur.fetchall()
+            return render_template('In-Outcome/outcome.html', rate_trans=rate_trans)
+
+@app.route('/save_outcome', methods=['POST'])
+def save_outcome():
+    with gobal.con:
+        cur = gobal.con.cursor()
+        if not session.get("name"):
+            return redirect("/login")
+        else:
+            sql = """INSERT INTO public.tb_outcome (item_name, cash_kip, cash_baht, cash_dollar, bill_date)
+                     VALUES(%s,%s,%s,%s, LOCALTIMESTAMP(0))
+                  """
+            item_name = request.form['item_name']
+            cash_kip = request.form['cash_kip']
+            cash_baht = request.form['cash_baht']
+            cash_dollar = request.form['cash_dollar']
+            bill_date = format(request.form['bill_date'])
+
+            data = (item_name, cash_kip, cash_baht, cash_dollar, bill_date)
+            print(data)
+            cur.execute(sql, (data))
+            gobal.con.commit()
+            return redirect(url_for('outcome'))
+
+@app.route('/outcome_delete/<string:id>')
+def outcome_delete(id):
+    if not session.get("name"):
+        return redirect("/login")
+    else:
+        print(id)
+        cur = gobal.con.cursor()
+        sql = "delete from public.tb_outcome where roworder=%s"
+        cur.execute(sql, (id,))
+        gobal.con.commit()
+        return redirect(url_for('outcome'))
+
