a95be648180cac4cf21c161a042800346b691b0f
keofc <keoitc2014@gmail.com>
2021-12-14 16:53:44 +0700

    ddd

--- app/exhange.py
+++ app/exhange.py
@@ -19,25 +19,27 @@ def homeex():
             rate_trans = cur.fetchall()
             return render_template('exchange/index.html', rate_trans=rate_trans)
 
-@app.route("/product/<id>")
-def product(id):
-    sql = """SELECT exchange_rate,ex_1,ex_2,key_value,date_end FROM (
-            select sale::text as exchange_rate,'K' as ex_1,'B' as ex_2,'01' as key_value,date_end  from public.exchange_rate where curency_code='01'
+
+@app.route("/product/<id>/<v>")
+def product(id, v):
+    sql = """SELECT exchange_rate,ex_1,ex_2,date_end FROM (
+            select sale::text as exchange_rate,'01' as ex_1,'00' as ex_2,date_end  from public.exchange_rate where curency_code='01'
             union all
-            select buy::text as exchange_rate,'B' as ex_1,'K' as ex_2,'02' as key_value,date_end from public.exchange_rate where curency_code='01'
+            select buy::text as exchange_rate,'00' as ex_1,'01' as ex_2,date_end from public.exchange_rate where curency_code='01'
             union all
-            select sale::text as exchange_rate,'K' as ex_1,'D' as ex_2,'03' as key_value,date_end from public.exchange_rate where curency_code='02'
+            select sale::text as exchange_rate,'01' as ex_1,'03' as ex_2,date_end from public.exchange_rate where curency_code='02'
             union all
-            select buy::text as exchange_rate,'D' as ex_1,'K' as ex_2,'04' as key_value,date_end from public.exchange_rate where curency_code='02'
+            select buy::text as exchange_rate,'03' as ex_1,'01' as ex_2,date_end from public.exchange_rate where curency_code='02'
             union all
-            select sale::text as exchange_rate,'B' as ex_1,'D' as ex_2,'05' as key_value,date_end from public.exchange_rate where curency_code='03'
+            select sale::text as exchange_rate,'00' as ex_1,'03' as ex_2,date_end from public.exchange_rate where curency_code='03'
             union all
-            select buy::text as exchange_rate,'D' as ex_1,'B' as ex_2,'06' as key_value,date_end from public.exchange_rate where curency_code='03') as a
-            where date_end isnull and key_value=%s"""
+            select buy::text as exchange_rate,'03' as ex_1,'00' as ex_2,date_end from public.exchange_rate where curency_code='03') as a
+            where date_end isnull and ex_1=%s and ex_2=%s"""
     cur = gobal.con.cursor()
-    cur.execute(sql, (id, ))
+    cur.execute(sql, (id, v))
     product = cur.fetchone()
     return jsonify({'product': product})
+
 
 @app.route('/save_ex', methods=['POST'])
 def save_ex():
@@ -49,7 +51,7 @@ def save_ex():
             sql = """INSERT INTO public.ic_trans(
                         ex_date, ex_1, ex_2, amount_1, exchange_rate, amount_2, tran_type,customer)
                         values(LOCALTIMESTAMP(0), %s, %s, %s, %s, %s,%s,%s)
-                """   
+                """
             customer_ = request.form['customer_']
             ex_1 = request.form['ex_1']
             ex_2 = request.form['ex_2']
@@ -57,14 +59,17 @@ def save_ex():
             exchange_rate = request.form['rate_show']
             amount_2 = request.form['tt_amount']
             tran_type = request.form['rate_code']
-            data = (ex_1, ex_2, amount_1, exchange_rate, amount_2, tran_type,customer_)
+            data = (ex_1, ex_2, amount_1, exchange_rate,
+                    amount_2, tran_type, customer_)
             if amount_1 != 0:
                 cur.execute(sql, (data))
                 gobal.con.commit()
                 flash('ບັນທຶກສຳເລັດ')
                 return redirect(url_for('homeex'))
-            else:     
+            else:
                 return redirect(url_for('homeex'))
+
+
 @app.route('/ex_delete/<string:id>')
 def ex_delete(id):
     with gobal.con:
@@ -76,4 +81,44 @@ def ex_delete(id):
             cur.execute(sql, (id,))
             gobal.con.commit()
             flash('ລົບສຳເລັດ ແລ້ວ')
+            return redirect(url_for('homeex'))
+
+
+@app.route('/sale')
+def sale():
+    with gobal.con:
+        cur = gobal.con.cursor()
+        if not session.get("name"):
+            return redirect("/login")
+        else:
+            cur = gobal.con.cursor()
+            sql = "SELECT bank_id, bank_name  FROM public.tb_bank order by roworder "
+            cur.execute(sql)
+            bank_from = cur.fetchall()
+            return render_template('exchange/sale.html',bank_from=bank_from)
+
+
+@app.route('/xchange_trans', methods=['POST'])
+def xchange_trans():
+    with gobal.con:
+        cur = gobal.con.cursor()
+        if not session.get("name"):
+            return redirect("/login")
+        else:
+            main_cur = request.form['main_cur']
+            rate = request.form['rate']
+            cash_recipt = request.form['cash_recipt']
+            trans_in = request.form['trans_in']
+            bank_amount = request.form['bank_amount']
+            total_amount1 = request.form['total_amount1']
+            second_cur = request.form['second_cur']
+            cash_pay = request.form['cash_pay']
+            bank_pay = request.form['bank_pay']
+            bank_account_code = request.form['bank_account_code']
+            bank_account_name = request.form['bank_account_name']
+            bank_pay_amount = request.form['bank_pay_amount']
+            fee = request.form['fee']
+            total_amount2 = request.form['total_amount2']
+            # bank_pay_amount = request.form['bank_pay_amount']
+
             return redirect(url_for('homeex'))
