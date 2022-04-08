8f949bc405273f59848bb082bbdeee3650f95f1c
Xaymon <xaymonmohn@gmail.com>
2022-04-08 10:59:56 +0700

    Try

--- kk_con.py
+++ kk_con.py
@@ -1,3 +1,3 @@
 import psycopg2
 class gobal:
-        con = psycopg2.connect(host="150.95.90.124", database="kk_exchange",user="postgres", password="sml", port="5432")
+        con = psycopg2.connect(host="46.252.181.107", database="bmtd3kmwtd3co2d74myb",user="uqowcfe9spmvmoy40pgk", password="czq4yqurzFrfXtmdmRQa", port="5432")
